854d17ea9ce69fef5e33281c8da456ad7d898e64
emreaslan7 <emreaslan7@icloud.com>
2024-09-21 10:54:40 +0300

    todolist part-2

--- todolist/App.js
+++ todolist/App.js
@@ -1,17 +1,89 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import Task from "./components/Task";
+import React, { useState } from "react";
+import { Keyboard } from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    Keyboard.dismiss();
+    let newTask = {
+      id: Math.random(),
+      text: task,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTask("");
+  };
+
+  const completeTask = (index) => {
+    let newTasks = [...tasks];
+    newTasks[index].isCompleted = !newTasks[index].isCompleted;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    let newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.taskBarWrapper}>
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
 
         <View style={styles.items}>
-          <TouchableOpacity style={styles.item}>
-            <Task text={"Task 1"} />
-          </TouchableOpacity>
+          {tasks.map((item, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => completeTask(index)}
+                style={styles.taskMenu}
+              >
+                <Task task={item} />
+                <TouchableOpacity
+                  style={styles.deleteIcon}
+                  onPress={() => {
+                    deleteTask(index);
+                  }}
+                >
+                  <MaterialIcons name="delete" size={24} color="#55BCF6" />
+                </TouchableOpacity>
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
+
+      <KeyboardAvoidingView
+        style={styles.writeTaskWrapper}
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          onChangeText={(text) => setTask(text)}
+          value={task}
+        />
+        <TouchableOpacity onPress={() => addTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -31,5 +103,48 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {
+    fontSize: 36,
+    color: "gray",
+    fontWeight: "200",
+  },
+  taskMenu: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  deleteIcon: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "#FFF",
   },
 });
--- todolist/components/Task.js
+++ todolist/components/Task.js
@@ -1,16 +1,30 @@
-// write Task component here
-
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function Task({ text }) {
+export default function Task({ task }) {
   return (
     <View style={styles.item}>
       <View style={styles.itemLeft}>
-        <TouchableOpacity style={styles.square}></TouchableOpacity>
-        <Text style={styles.itemText}>{text}</Text>
+        {task.isCompleted ? (
+          <MaterialIcons
+            name="check"
+            size={24}
+            color="green"
+            style={styles.checkIcon}
+          /> // Tik ikonu
+        ) : (
+          <View style={styles.square} />
+        )}
+        <Text
+          style={[
+            styles.itemText,
+            { textDecorationLine: task.isCompleted ? "line-through" : "none" },
+          ]}
+        >
+          {task.text}
+        </Text>
       </View>
-      <View style={styles.circular}></View>
     </View>
   );
 }
@@ -22,13 +36,12 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
   },
   itemLeft: {
     flexDirection: "row",
     alignItems: "center",
     flexWrap: "wrap",
+    width: "80%",
   },
   square: {
     width: 24,
@@ -41,11 +54,7 @@ const styles = StyleSheet.create({
   itemText: {
     maxWidth: "80%",
   },
-  circular: {
-    width: 12,
-    height: 12,
-    borderColor: "#55BCF6",
-    borderWidth: 2,
-    borderRadius: 5,
+  checkIcon: {
+    marginRight: 15,
   },
 });
