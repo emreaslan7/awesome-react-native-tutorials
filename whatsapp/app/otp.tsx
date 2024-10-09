8812039871ddea475dbd55b966cdbaff14b79838
emreaslan7 <emreaslan7@icloud.com>
2024-10-09 23:41:35 +0300

    otp clerk

--- whatsapp/app/otp.tsx
+++ whatsapp/app/otp.tsx
@@ -1,18 +1,24 @@
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { useState } from "react";
 import {
   View,
   Text,
-  Platform,
   StyleSheet,
-  TextInput,
+  Linking,
   TouchableOpacity,
   KeyboardAvoidingView,
+  Platform,
   ActivityIndicator,
+  Alert,
 } from "react-native";
-import React from "react";
-import { useRouter } from "expo-router";
-import Colors from "@/constants/Colors";
-import { Ionicons } from "@expo/vector-icons";
 import MaskInput from "react-native-mask-input";
+import {
+  isClerkAPIResponseError,
+  useSignIn,
+  useSignUp,
+} from "@clerk/clerk-expo";
 
 const GER_PHONE = [
   `+`,
@@ -34,35 +40,96 @@ const GER_PHONE = [
 ];
 
 const Page = () => {
-  const [loading, setLoading] = React.useState(false);
-  const [phoneNumber, setPhoneNumber] = React.useState("");
-  const router = useRouter();
+  const [phoneNumber, setPhoneNumber] = useState("");
   const keyboardVerticalOffset = Platform.OS === "ios" ? 90 : 0;
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { signUp, setActive } = useSignUp();
+  const { signIn } = useSignIn();
 
-  const openLink = () => {};
+  const openLink = () => {
+    Linking.openURL("https://galaxies.dev");
+  };
 
-  const sendOTP = async () => {};
+  const sendOTP = async () => {
+    console.log("sendOTP", phoneNumber);
+    setLoading(true);
 
-  const trySignIn = async () => {};
+    try {
+      await signUp!.create({
+        phoneNumber,
+      });
+      console.log("TESafter createT: ", signUp!.createdSessionId);
+
+      signUp!.preparePhoneNumberVerification();
+
+      console.log("after prepare: ");
+      router.push(`/verify/${phoneNumber}`);
+    } catch (err) {
+      console.log("error", JSON.stringify(err, null, 2));
+
+      if (isClerkAPIResponseError(err)) {
+        if (err.errors[0].code === "form_identifier_exists") {
+          // User signed up before
+          console.log("User signed up before");
+          await trySignIn();
+        } else {
+          setLoading(false);
+          Alert.alert("Error", err.errors[0].message);
+        }
+      }
+    }
+  };
+
+  const trySignIn = async () => {
+    console.log("trySignIn", phoneNumber);
+
+    const { supportedFirstFactors } = await signIn!.create({
+      identifier: phoneNumber,
+    });
+
+    // @ts-ignore
+
+    const firstPhoneFactor: any = supportedFirstFactors.find((factor: any) => {
+      return factor.strategy === "phone_code";
+    });
+
+    const { phoneNumberId } = firstPhoneFactor;
+
+    await signIn!.prepareFirstFactor({
+      strategy: "phone_code",
+      phoneNumberId,
+    });
+
+    router.push(`/verify/${phoneNumber}?signin=true` as any);
+    setLoading(false);
+  };
+
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={keyboardVerticalOffset}
+      style={{ flex: 1 }}
+      behavior="padding"
+    >
       {loading && (
         <View style={[StyleSheet.absoluteFill, styles.loading]}>
           <ActivityIndicator size="large" color={Colors.primary} />
           <Text style={{ fontSize: 18, padding: 10 }}>Sending code...</Text>
         </View>
       )}
+
       <View style={styles.container}>
         <Text style={styles.description}>
-          Whatsapp will need to verify your account. Carrier changes may apply.
+          WhatsApp will need to verify your account. Carrier charges may apply.
         </Text>
 
         <View style={styles.list}>
           <View style={styles.listItem}>
-            <Text style={styles.listItemText}>Country/Region</Text>
+            <Text style={styles.listItemText}>Germany</Text>
             <Ionicons name="chevron-forward" size={20} color={Colors.gray} />
           </View>
           <View style={styles.separator} />
+
           <MaskInput
             value={phoneNumber}
             keyboardType="numeric"
